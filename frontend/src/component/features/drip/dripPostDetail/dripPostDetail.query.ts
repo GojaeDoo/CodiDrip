9f2300e2dceaa8c8eb8f75b3fb766e946d9add75
gojaedoo <minchoi9979@naver.com>
2025-07-03 15:11:47 +0900

    상세페이지 에러 조치

--- frontend/src/component/features/drip/dripPostDetail/dripPostDetail.query.ts
+++ frontend/src/component/features/drip/dripPostDetail/dripPostDetail.query.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { DripPostDetailResponse } from "./DripPostDetail.types";
+import { DripPostDetailResponse } from "./dripPostDetail.types";
 
 export const getDripPostDetail = async (
   postNo: number
