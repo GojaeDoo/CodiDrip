58bc0781a829709406f7edb76aa0f08049d7bac6
gojaedoo <minchoi9979@naver.com>
2025-07-03 14:30:52 +0900

    배포 에러 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // 강제 리렌더링 방지
   compiler: {
     styledComponents: true, // styled-components 활성화
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
   images: {
     remotePatterns: [
